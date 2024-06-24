024c2bd56475cc87cc771dd0af286a653e18e167
Pr0gramm9r <krashbox3@gmail.com>
2024-06-24 14:56:19 +0300

    v.1.0.0

--- Ping pong.py
+++ Ping pong.py
@@ -51,7 +51,7 @@ class Ball(Base_sprite):
             self.rect.x = win_w/2
             self.rect.y = win_h/2
             lifes2 = lifes2 - 1
- 
+
 
 win_w = 700    
 win_h = 500
@@ -64,8 +64,8 @@ lifes2 = 3
 
 pg.font.init()
 font1 = pg.font.SysFont('Arial', 40)
-win1 = font1.render('1 player, you win', True, (16, 227, 139))
-win2 = font1.render('2 player, you win', True, (16, 227, 139))
+win1 = font1.render('1 player, you win', True, (240, 232, 10))
+win2 = font1.render('2 player, you win', True, (240, 232, 10))
 lose1 = font1.render('1 player, you lose', True, (235, 19, 19))
 lose2 = font1.render('2 player, you lose', True, (235, 19, 19))
 font2 = pg.font.SysFont('Arial', 36)
@@ -81,12 +81,15 @@ rocket2 = Rocket('ping_rocket2.png', 640, 200, 25, 100, 4)
 
 ball = Ball('ball.png', win_w/2, win_h/2, 50, 50, 2)
 
-first_heart1 = Base_sprite('life1.png', win_w - 100, 15, 70, 50, 0)
-first_heart2 = Base_sprite('life2.png', win_w - 170, 20, 125, 45, 0)
-first_heart3 = Base_sprite('life3.png', win_w - 210, 20, 200, 40, 0)
-second_heart1 = Base_sprite('life1.png', 10, 20, 70, 50, 0)
-second_heart2 = Base_sprite('life2.png', 10, 20, 125, 45, 0)
-second_heart3 = Base_sprite('life3.png', 10, 20, 200, 40, 0)
+rocket2_heart1 = Base_sprite('life1.png', win_w - 60, 20, 50, 40, 0)
+rocket2_heart2 = Base_sprite('life1.png', win_w - 110, 20, 50, 40, 0)
+rocket2_heart3 = Base_sprite('life1.png', win_w - 160, 20, 50, 40, 0)
+rocket1_heart1 = Base_sprite('life1.png', 10, 20, 50, 40, 0)
+rocket1_heart2 = Base_sprite('life1.png', 60, 20, 50, 40, 0)
+rocket1_heart3 = Base_sprite('life1.png', 110, 20, 50, 40, 0)
+
+pg.mixer.init()
+bounce = pg.mixer.Sound('schelchok-vklyucheniya.ogg')
 
 play = True
 game_finish = False
@@ -106,26 +109,39 @@ while play:
 
         if ball.rect.y >= win_h - 50 or ball.rect.y <= 0:
             dy *= -1
+            bounce.play()
+
 
         if ball.rect.colliderect(rocket1.rect) or ball.rect.colliderect(rocket2.rect):
             dx *= -1
+            bounce.play()
 
 
         if lifes1 == 3:
-            first_heart3.draw()
+            rocket2_heart1.draw()
+            rocket2_heart2.draw()
+            rocket2_heart3.draw()
+
         if lifes1 == 2:
-            first_heart2.draw()
+            rocket2_heart1.draw()
+            rocket2_heart2.draw()
+
         if lifes1 == 1:
-            first_heart1.draw()
+            rocket2_heart1.draw()
+     
         
-
         if lifes2 == 3:
-            second_heart3.draw()
+            rocket1_heart1.draw()
+            rocket1_heart2.draw()
+            rocket1_heart3.draw()
+            
         if lifes2 == 2:
-            second_heart2.draw()
+            rocket1_heart1.draw()
+            rocket1_heart2.draw()
+            
         if lifes2 == 1:
-            second_heart1.draw()
-
+            rocket1_heart1.draw()
+            
 
         ball.update()
         rocket1.update_l()
@@ -136,17 +152,25 @@ while play:
         ball.draw()
 
 
-        if lifes1 <= 0:
+        if lifes2 <= 0:
+            mw.blit(fon, (0,0))
 
             game_finish = True
             mw.blit(lose1, (30,80))
-            mw.blit(win2, (350, 80))
+            mw.blit(win2, (355, 80))
 
-        if lifes2 <= 0:
+        if lifes1 <= 0:
+            mw.blit(fon, (0,0))
 
             game_finish = True
-            mw.blit(lose2, (350,80))
+            mw.blit(lose2, (355,80))
             mw.blit(win1, (30,80)) 
+
+    else:
+        pg.time.delay(4000)
+        finish = False
+        lifes1 = 3
+        lifes2 = 3
 
 
     pg.display.update()
